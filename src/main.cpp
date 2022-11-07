12f851e4e01ebcf64b8d7ed8102d996224459c5f
RFlintstone <rubenflint@hotmail.com>
2022-11-07 16:23:38 +0100

    All color values are now send through bluetooth

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,12 @@
 #include <Wire.h>
+#include <WiFi.h>
 #include "../lib/BluetoothSerial/src/BluetoothSerial.h"
 #include "../lib/Adafruit_BusIO/Adafruit_I2CDevice.h"
 #include "../lib/Adafruit_BusIO/Adafruit_SPIDevice.h"
 #include "../lib/Adafruit_TCS34725/Adafruit_TCS34725.h"
 
 #if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
-#error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
+#error Bluetooth is not enabled! Please run 'make menuconfig' to and enable it
 #endif
 
 // Define Bluetooth serial
@@ -17,7 +18,7 @@ Adafruit_TCS34725 tcs;
 // Signatures
 void setColorSensor();
 
-void getColor();
+std::initializer_list<unsigned short> getColor();
 
 /**
  * Start and configure our:
@@ -28,12 +29,28 @@ void setup() {
     Serial.begin(115200);
     SerialBT.begin("ColorSensor");
     setColorSensor();
+    Serial.println(WiFi.macAddress());
 }
 
 /**
  * Refresh data
  */
 void loop() {
+    auto color= getColor();
+
+    auto r = (String) color.begin()[0];
+    auto g = (String) color.begin()[1];
+    auto b = (String) color.begin()[2];
+    auto c = (String) color.begin()[3];
+    auto temp = (String) color.begin()[4];
+    auto lux = (String) color.begin()[5];
+
+    String a = "R: " + r + ", G: " + g + ", B: " + b + ", C: " + c + ", TEMP: " + temp + ", LUX" + lux;
+    uint8_t buf[a.length()];
+    memcpy(buf,a.c_str(),a.length());
+    SerialBT.write(buf,a.length());
+    SerialBT.println();
+
     if (Serial.available()){
         SerialBT.write(Serial.read());
     }
@@ -41,8 +58,7 @@ void loop() {
     if (SerialBT.available()){
         Serial.write(SerialBT.read());
     }
-
-//    getColor();
+    delay(1000);
 }
 
 /**
@@ -66,7 +82,7 @@ void setColorSensor() {
 /**
  * Get/change the current colour
  */
-void getColor() {
+std::initializer_list<unsigned short> getColor() {
     // Create unsigned variables to store our values in
     uint16_t r, g, b, c, colorTemp, lux;
 
@@ -75,31 +91,31 @@ void getColor() {
     lux = tcs.calculateLux(r, g, b);                        // Calculate LUX
 
     // Print the values to the serial monitor
-    Serial.print("RAW R: ");
-    Serial.print(r, DEC);
-    Serial.print(" ");
-    Serial.print("G: ");
-    Serial.print(g, DEC);
-    Serial.print(" ");
-    Serial.print("B: ");
-    Serial.print(b, DEC);
-    Serial.println(" ");
+//    Serial.print("RAW R: ");
+//    Serial.print(r, DEC);
+//    Serial.print(" ");
+//    Serial.print("G: ");
+//    Serial.print(g, DEC);
+//    Serial.print(" ");
+//    Serial.print("B: ");
+//    Serial.print(b, DEC);
+//    Serial.println(" ");
 
-    if (r >= 255 && g >= 255 && b >= 255) {
-        Serial.println("White");
-    } else if (r >= 255 && g >= 255 && b <= 255) {
-        Serial.println("Yellow");
-    } else if (r >= 255 && g <= 255 && b <= 255) {
-        Serial.println("Red");
-    } else if (r <= 255 && g >= 255 && b <= 255) {
-        Serial.println("Green");
-    } else if (r <= 255 && g <= 255 && b >= 255) {
-        Serial.println("Blue");
-    } else if (r <= 0 && g <= 0 && b <= 0) {
-        Serial.println("Black");
-    } else {
-        Serial.println("Unknown");
-    }
+//    if (r >= 255 && g >= 255 && b >= 255) {
+//        Serial.println("White");
+//    } else if (r >= 255 && g >= 255 && b <= 255) {
+//        Serial.println("Yellow");
+//    } else if (r >= 255 && g <= 255 && b <= 255) {
+//        Serial.println("Red");
+//    } else if (r <= 255 && g >= 255 && b <= 255) {
+//        Serial.println("Green");
+//    } else if (r <= 255 && g <= 255 && b >= 255) {
+//        Serial.println("Blue");
+//    } else if (r <= 0 && g <= 0 && b <= 0) {
+//        Serial.println("Black");
+//    } else {
+//        Serial.println("Unknown");
+//    }
 
 //    Serial.print("Color Temp: ");
 //    Serial.print(colorTemp, DEC);
@@ -120,4 +136,7 @@ void getColor() {
 //    Serial.print(c, DEC);
 //    Serial.print(" ");
 //    Serial.println(" ");
+
+    auto res = {r, g, b, c, colorTemp, lux};
+    return res;
 }
