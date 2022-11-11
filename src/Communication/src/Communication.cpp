9f6138c1cc5f05c5b91cf9ab66220aeb58fc558a
RFlintstone <rubenflint@hotmail.com>
2022-11-11 21:19:13 +0100

    Added more comments

--- src/Communication/src/Communication.cpp
+++ src/Communication/src/Communication.cpp
@@ -10,32 +10,51 @@
 #include "../../../lib/ESP32_BLE_Arduino-1.0.1/src/BLE2902.h"
 //#include "../../../lib/BluetoothSerial/src/BluetoothSerial.h"
 
+// Make sure Hardware Serial can be used in the file
 extern HardwareSerial Serial;
 
+// Define our *static* UUIDs to start our Bluetooth instance
 #define SERVICE_UUID        "d6960225-2fbc-4565-bf52-0f1cc61bbfcf"
 #define CHARACTERISTIC_UUID "13d258f5-80c4-4863-a995-53604358e2e5"
 
+// Declare our Bluetooth server and Characteristics
 BLEServer *pServer = NULL;
 BLECharacteristic *pCharacteristic = NULL;
 
+// Declare variables so we can check or bluetooth connection status
 bool deviceConnected;
 bool oldDeviceConnected;
-
 uint32_t value = 0;
 
+/**
+ * Define our Bluetooth callback.
+ */
 class MyServerCallbacks : public BLEServerCallbacks {
+    /**
+     * Run this function when a new device connected to our ESP
+     * @param pServer
+     */
     void onConnect(BLEServer *pServer) {
         deviceConnected = true;
-        Serial.println("New connection detected: " + pServer->getConnId());
+        Serial.println("New connection established");
     };
 
+    /**
+     * Run this function when a device disconnected from our ESP
+     * @param pServer
+     */
     void onDisconnect(BLEServer *pServer) {
         deviceConnected = false;
-        Serial.println("Connection dropped..");
+        Serial.println("Dropped current connection..");
     }
 };
 
+/**
+ * Define our startup sequence for a bluetooth connection
+ * This needs to be run in setup()
+ */
 void Communication::setBT() {
+    // Reset our bluetooth properties when we run the setup on a restart
     deviceConnected = false;
     oldDeviceConnected = false;
     value = 0;
@@ -59,15 +78,15 @@ void Communication::setBT() {
             BLECharacteristic::PROPERTY_INDICATE
     );
 
-    // https://www.bluetooth.com/specifications/gatt/viewer?attributeXmlFile=org.bluetooth.descriptor.gatt.client_characteristic_configuration.xml
     // Create a BLE Descriptor
     pCharacteristic->addDescriptor(new BLE2902());
 
-    // Start the service
+    // Start the Bluetooth service
     pService->start();
 
-    // Start advertising
+    // Start advertising our service
     pServer->getAdvertising()->start();
+
 //    BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
 //    pAdvertising->addServiceUUID(SERVICE_UUID);
 //    pAdvertising->setScanResponse(false);
@@ -75,75 +94,55 @@ void Communication::setBT() {
 //    BLEDevice::startAdvertising();
 }
 
+/**
+ * Send values to our connected device, over Bluetooth
+ * This function needs to be run in loop()
+ * @param list
+ */
 void Communication::writeBT(int *list) {
-    int *color = list;
-    String r = (String) color[0];
-    String g = (String) color[1];
-    String b = (String) color[2];
-    String c = (String) color[3];
-    String temp = (String) color[4];
-    String lux = (String) color[5];
+    // Store String versions of the ints in the passed array in new variables
+    String r = (String) list[0];
+    String g = (String) list[1];
+    String b = (String) list[2];
+    String c = (String) list[3];
+    String temp = (String) list[4];
+    String lux = (String) list[5];
 
-//    auto a = "R: " + r + ", G: " + g + ", B: " + b + ", C: " + c + ", TEMP: " + temp + ", LUX: " + lux;
-//    Serial.println(a);
-
-    uint8_t val2[3] = {254, 0, 0};
-    pCharacteristic->setValue((uint8_t *) &val2, 3);
+    // Prepare and send a value
+    uint8_t val[3] = {255, 0, 0};
+    pCharacteristic->setValue((uint8_t *) &val, 3);
     pCharacteristic->notify();
 
-    uint8_t val3[5] = {'R', 'U', 'B', 'E', 'N'};
-    pCharacteristic->setValue((uint8_t *) &val3, 5);
+    // Prepare and send a value
+    uint8_t val1[5] = {'R', 'U', 'B', 'E', 'N'};
+    pCharacteristic->setValue((uint8_t *) &val1, 5);
     pCharacteristic->notify();
 
-    // notify changed value
-    if (deviceConnected) {
-        Serial.println("Device connected!");
-
-//        pCharacteristic->setValue((uint8_t *) &value, 4);
-//        pCharacteristic->notify();
-//        value++;
-        delay(3); // bluetooth stack will go into congestion, if too many packets are sent, in 6 hours test i was able to go as low as 3ms
-    }
-
+    // While disconnected
     if (!deviceConnected) {
         Serial.println("No device connected!");
     }
 
-    // disconnecting
+    // While connected
+    if (deviceConnected) {
+        Serial.println("Device connected!");
+
+        // Bug: (prepared) messages won't send from inside an if statement(?)
+
+        delay(3); // bluetooth stack will go into congestion, if too many packets are sent, in 6 hours test i was able to go as low as 3ms
+    }
+
+    // Disconnecting
     if (!deviceConnected && oldDeviceConnected) {
         delay(500); // give the bluetooth stack the chance to get things ready
         pServer->startAdvertising(); // restart advertising
-        Serial.println("start advertising");
-        oldDeviceConnected = deviceConnected;
+        Serial.println("Restarting advertising..");
+        oldDeviceConnected = deviceConnected; // Set oldDeviceConnected to false
     }
 
-    // connecting
+    // Connecting
     if (deviceConnected && !oldDeviceConnected) {
-        // do stuff here on connecting
-        oldDeviceConnected = deviceConnected;
+        Serial.println("Connecting to a new device..");
+        oldDeviceConnected = deviceConnected; // If we connect to a device, set oldDeviceConnected to true
     }
-
-//    auto color = list;
-//    auto r = (String) color.begin()[0];
-//    auto g = (String) color.begin()[1];
-//    auto b = (String) color.begin()[2];
-//    auto c = (String) color.begin()[3];
-//    auto temp = (String) color.begin()[4];
-//    auto lux = (String) color.begin()[5];
-//
-//    String a = "R: " + r + ", G: " + g + ", B: " + b + ", C: " + c + ", TEMP: " + temp + ", LUX: " + lux;
-//    uint8_t buf[a.length()];
-//    memcpy(buf, a.c_str(), a.length());
-//    SerialBT.write(buf, a.length());
-//    SerialBT.println();
-//
-//    if (SerialBT.connected()) {
-//        if (Serial.available()) {
-//            SerialBT.write(Serial.read());
-//        }
-//
-//        if (SerialBT.available()) {
-//            Serial.write(SerialBT.read());
-//        }
-//    }
 }
